4f2c1380a4da215dda5abfee6f3e1666e199af55
gfvxgd2k <gfvxgd2k@gmail.com>
2023-05-21 23:49:58 +0900

    add vectorization operation

--- chapter_1/1_2_1_check_prime.py
+++ chapter_1/1_2_1_check_prime.py
@@ -7,6 +7,16 @@ def check_prime(number):
             return False
     return True
 
+def check_prime_better(number):
+    #FIXME must be fixed after chapter 6
+    sqrt_number = math.sqrt(number)
+    numbers = range(2, int(sqrt_number) + 1)
+    for i in range(0, len(numbers), 5):
+        result = (number / numbers[i:(i + 5)]).is_integer()
+        if any(result):
+            return False
+    return True
+
 if __name__ == '__main__':
     print(f'check_prime(10,000,000) = {check_prime(10_000_000)}')
     print(f'check_prime(10,000,019) = {check_prime(10_000_019)}')
